165834ddb82f5655ea504abca7e65d8d4f158ed5
Katherine Sweet <katherine.sweet@slu.edu>
2020-02-06 10:38:04 -0600

    Finished SQL queries

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -27,6 +27,14 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
     public Employer() {}
 
 
